ea916c232dace3fa52cb75742872f726d1fcba23
takane2497 <takane.konagaya@gmail.com>
2022-10-20 06:17:35 +0900

    libft03

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takonaga <takonaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 05:52:28 by takonaga          #+#    #+#             */
+/*   Updated: 2022/10/20 06:10:06 by takonaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	str = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i++] != '\0')
+		str[i] = f(i, s[i]);
+	return (str);
+}
