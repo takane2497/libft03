58115da0c2878f4daeaa611a3fcb736d28354f58
takane2497 <takane.konagaya@gmail.com>
2022-10-22 21:53:02 +0900

    libft03 機械採点後修正

--- ft_split.c
+++ ft_split.c
@@ -6,79 +6,85 @@
 /*   By: takonaga <takonaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 17:36:56 by takonaga          #+#    #+#             */
-/*   Updated: 2022/10/21 00:26:25 by takonaga         ###   ########.fr       */
+/*   Updated: 2022/10/22 21:52:31 by takonaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_words(char const *s, char c)
+static size_t	count_word(char const *str, char c)
 {
-	size_t	words;
 	size_t	i;
+	size_t	num_word;
 
-	words = 0;
 	i = 0;
-	while (s[i] != '\0' && s[i + 1] != '\0')
+	num_word = 0;
+	while (str[i] != '\0')
 	{
-		if (s[i] == c && s[i + 1] != c)
-			words++;
-		i++;
+		while (str[i] == c && str[i] != '\0')
+			i++;
+		if (str[i] != c && str[i] != '\0')
+		{
+			while (str[i] != c && str[i] != '\0')
+				i++;
+			num_word++;
+		}
 	}
-	return (words);
+	return (num_word);
 }
 
-static size_t	move_len(const char *s, char c, int start)
+char	*next_str(char *s, char c)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[start + i] != c && s[start + i] != '\0')
+	while (s[i] != '\0' && s[i] == c)
 		i++;
-	return (i);
+	s = s + i;
+	return (s);
 }
 
-static char	**allfree(char **ptr)
+static char	**allfree(char **rtn_str)
 {
 	size_t	i;
 
 	i = 0;
-	while (ptr[i] != NULL)
+	while (rtn_str[i] != NULL)
 	{
-		free(ptr[i]);
+		free(rtn_str[i]);
 		i++;
 	}
-	free(ptr);
+	free(rtn_str);
 	return (NULL);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**ptr;
+	char	**result_str;
 	size_t	i;
-	size_t	start;
+	size_t	j;
+	size_t	word;
 
 	if (s == NULL)
 		return (NULL);
-	ptr = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (ptr == NULL)
+	word = count_word(s, c);
+	result_str = (char **) malloc (sizeof(char *) * (word + 1));
+	if (result_str == NULL)
 		return (NULL);
-	i = 0;
-	start = 0;
-	while (s[i] != '\0')
+	j = 0;
+	while (j < word)
 	{
-		if (s[i] != c)
-		{
-			ptr[start] = ft_substr(s, i, move_len(s, c, i));
-			if (ptr[start++] == NULL)
-				return (allfree(ptr));
-			i += move_len(s, c, i);
-		}
-		else
+		i = 0;
+		s = next_str((char *)s, c);
+		while (((char *)s)[i] != '\0' && ((char *)s)[i] != c)
 			i++;
+		result_str[j] = ft_substr(s, 0, i);
+		if (result_str[j++] == NULL)
+			return (allfree(result_str));
+		s = s + i;
 	}
-	ptr[start] = NULL;
-	return (ptr);
+	result_str[j] = NULL;
+	return (result_str);
 }
 
 // int main(int argc, char **argv) {
